dba482ceca0ba7b31d1c255a751526d3a6b42456
yuya1212h4 <yuya1212h4.066@gmail.com>
2022-01-30 23:43:55 +0900

    add graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./src/**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.schema.ts'),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
